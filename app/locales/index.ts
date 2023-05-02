402c86546f9176bebdc3514350dd968a96ea8e73
Michael Sun <108275254+MichaelSunMeta@users.noreply.github.com>
2023-05-02 15:07:47 +0800

    Update index.ts

--- app/locales/index.ts
+++ app/locales/index.ts
@@ -1,11 +1,11 @@
 import CN from "./cn";
 import EN from "./en";
 import TW from "./tw";
-import ES from "./es";
-import IT from "./it";
-import TR from "./tr";
-import JP from "./jp";
-import DE from "./de";
+//import ES from "./es";
+//import IT from "./it";
+//import TR from "./tr";
+//import JP from "./jp";
+//import DE from "./de";
 
 export type { LocaleType } from "./cn";
 
@@ -13,11 +13,11 @@ export const AllLangs = [
   "en",
   "cn",
   "tw",
-  "es",
-  "it",
-  "tr",
-  "jp",
-  "de",
+//  "es",
+//  "it",
+//  "tr",
+//  "jp",
+//  "de",
 ] as const;
 export type Lang = (typeof AllLangs)[number];
 
@@ -72,9 +72,9 @@ export default {
   en: EN,
   cn: CN,
   tw: TW,
-  es: ES,
-  it: IT,
-  tr: TR,
-  jp: JP,
-  de: DE,
+//  es: ES,
+//  it: IT,
+//  tr: TR,
+//  jp: JP,
+//  de: DE,
 }[getLang()] as typeof CN;
